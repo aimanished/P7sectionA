447de36323275f27200c196bfbb9616fd1529b20
Aiman Danish <aimanished@gmail.com>
2018-06-19 15:19:50 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/p7sectiona/MainActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.webservices.p7sectiona;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment f1 = new Fragment_first();
+        ft.replace(R.id.frame1,f1);
+
+        ft.commit();
+    }
+}
